007695a3248c65c76c38ee169c9db9547ea78fbc
KoroNeko0 <memo33300@gmail.com>
2024-10-14 19:42:52 +0300

    Test

---
+++ src/Manager.java
@@ -0,0 +1,21 @@
+
+public class Manager {
+	
+	//Attributes:
+	private String name;
+	private int years;
+	
+	//Constructor:
+	public Manager(String name , int years) {
+		
+		this.name = name;
+		this.years = years;
+		
+	}
+	
+	public String toString() {
+		
+		return "Shop's Manager Name : "+name +"\nAge : "+years+" years\n\n";
+	}
+	
+}//end class
